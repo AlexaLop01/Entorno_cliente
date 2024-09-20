454f1fd2723021bfa134a417cd55ded5ed504a57
usuario <correo>
2024-09-20 19:24:20 +0200

    Cambios en la práctica2.01 del ejercicio03

--- practica2.01/ejercicio03/ejercicio03.js
+++ practica2.01/ejercicio03/ejercicio03.js
@@ -3,12 +3,14 @@
 //Definimos la función que vamos a utilizar para calcular cuantas repeticiones y por cuanto debe multiplicarse.
 function interaccionMultiplicar(repeticiones, interaccion){
     //Para mostrar la iteracción
-    console.log(interaccion);
+    console.log(`El primer número es: ${interaccion}`);
     //Usamos el for para sacar los numeros multiplicados
     for (var i = 1; i < repeticiones ; i++) {
-        console.log(interaccion *=2);
+        interaccion*=2;
+        console.log(`Siguiente número multiplicado por 2: ${interaccion}`);
     }
 }
+
 
 //hacemos la prueba de salida por consola.
 interaccionMultiplicar(4,6);
