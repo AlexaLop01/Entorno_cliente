5728769ea50ee3e8941e434744cf5728d05d5118
usuario <correo>
2024-09-24 20:46:51 +0200

    cambios en las prácticas

--- practica2.03/ejercicio03/ejercicio03.js
+++ practica2.03/ejercicio03/ejercicio03.js
@@ -6,7 +6,7 @@ function discente(id, nom, apellidos, nota1, nota2, nota3) {
         id: id,
         nombre: nom,
         apellidos: apellidos,
-        aficiones: [],
+        aficiones: ["musica", "fútbol", "baile"],
         notas:{
             primera: nota1,
             segunda: nota2,
@@ -15,6 +15,17 @@ function discente(id, nom, apellidos, nota1, nota2, nota3) {
     }
 }
 
+function calcularMedia(notas) {
+    console.log((discente.notas.nota1 + discente.notas.nota1 + discente.notas.nota1)/ 3);
+}
+
+let disciente1 = discente("01", "Manuel", "martinez", 5, 6, 8);
+
+function imprimirAficiones(disciente){
+    console.log(disciente.aficiones);
+}
+imprimirAficiones(disciente1);
+calcularMedia(disciente1);
 //función para calcular la media de las 3 evaluaciones
 
 //función para imprimir las aficiones
