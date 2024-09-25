455a65ecad895a3af9fc6c02db2ddd33f812a824
AlexaLop01 <alexandralop107@gmail.com>
2024-09-25 12:46:53 +0200

    Cambios en la práctica 3

--- practica2.03/ejercicio03/ejercicio03.js
+++ practica2.03/ejercicio03/ejercicio03.js
@@ -11,23 +11,49 @@ function discente(id, nom, apellidos, nota1, nota2, nota3) {
             primera: nota1,
             segunda: nota2,
             tercera: nota3
+        },
+
+        //función para calcular la media de las 3 evaluaciones
+        calcularMedia : function() {
+            let suma = this.notas.primera + this.notas.segunda + this.notas.tercera;
+            return suma/3;
+        },
+
+        //función para imprimir las aficiones
+        imprimirAficiones : function(){
+            console.log(`Las aficiones de ${this.nombre} son: `);
+            
+            let resultadoAficiones = this.aficiones.map((aficion) => {
+                console.log(`${aficion}`);
+                return aficion;
+                
+            });
+        },
+
+        //función para imprimir el informe completo
+        imprimirInforme: function() {
+            console.log(`\n\tInforme del discente`);
+            console.log(`----------------------------------`);
+            console.log(`ID: ${this.id}`);
+            console.log(`Nombre completo: ${this.nombre} ${this.apellidos}`);
+            console.log(`Notas de evaluación: `);
+            console.log(`\t·Primera: ${this.notas.primera}`);
+            console.log(`\t·Segunda: ${this.notas.segunda}`);
+            console.log(`\t·Tercera: ${this.notas.tercera}`);
+            console.log(`\t·Nota media: ${this.calcularMedia().toFixed(2)}`);
+            this.imprimirAficiones();
+            
         }
+        
     }
 }
 
-function calcularMedia(notas) {
-    console.log((discente.notas.nota1 + discente.notas.nota1 + discente.notas.nota1)/ 3);
-}
 
-let disciente1 = discente("01", "Manuel", "martinez", 5, 6, 8);
 
-function imprimirAficiones(disciente){
-    console.log(disciente.aficiones);
-}
-imprimirAficiones(disciente1);
-calcularMedia(disciente1);
-//función para calcular la media de las 3 evaluaciones
+let discente1 = discente("01", "Manuel", "Martinez Gomez", 5, 6, 8);
+let discente2 = discente("02", "María", "Gonzalez López", 8, 7, 8);
 
-//función para imprimir las aficiones
+discente1.imprimirInforme();
+discente2.imprimirInforme();
 
-//función para imprimir el informe completo
+
