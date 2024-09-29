7fad53da20ea613f2c0c0f367284a7bbcf6e7ba4
AlexaLop01 <alexandralop107@gmail.com>
2024-09-29 15:22:09 +0200

    Cambios en el ejercicio 1 y la biblioteca

--- practica2.04/biblioteca/biblioteca.js
+++ practica2.04/biblioteca/biblioteca.js
@@ -4,11 +4,72 @@
 //Acordarnos de que hay que exportarlas para poder utilizarlas en los otros ficheros js.
 //Para exportar : export { nombreFuncion };.
 
+//Función cambiar a mayúsculas.
 function cambiarMayusculas(array) {
-    let arraMayusculas = array.map((nombre) => {
+    let arrayMayusculas = [...array].map((nombre) => { 
         return nombre.toUpperCase(); 
     });
-    return `El array en mayúsculas es : [${arraMayusculas}]`;
+    return `El array en mayúsculas es : ${arrayMayusculas}`;
 }
 
-export {cambiarMayusculas};
+//Función para ordenar alfabéticamente al revés.
+function ordenarAlfabetoReves(array) {
+    let nombreAlfabeto = [...array].sort(); //Ordenamos primero el array alfabéticamente.
+    nombreAlfabeto = nombreAlfabeto.reverse(); //Colocamos el array de forma inversa.
+    return `El array ordenado alfabéticamente invertido es: ${nombreAlfabeto}`;
+}
+
+//Función para mostrar los elementos de un objeto. Reutilizo la función de la práctica anterior.
+function mostrarObjeto(objeto) {
+    if (typeof objeto !== "object" || objeto === null) {
+      console.log(`El parámetro introducido no es un objeto.`);
+    }
+  
+    let datosObjeto;
+  
+    for (var clave in objeto) {
+      //Comprobamos que el objeto tenga la propiedad
+      if (objeto.hasOwnProperty(clave)) {
+        let valor = objeto[clave]; //guardamos el valor de la propiedad en una variable
+        let tipoDato = typeof valor; // guardamos el tipo de dato del valor en una variable
+  
+        //Los if o if else son para verificar que tipo de dato son y si cumple la condición saca por consola lo que le corresponde.
+        if (tipoDato === "number") {
+          console.log(`${clave} : ${valor}.`);
+        } else if (tipoDato === "string") {
+          console.log(`${clave} : ${valor}.`);
+        } else if (Array.isArray(valor)) {
+          console.log(`${clave} : [${valor}]`);
+        } else if (tipoDato === "object") {
+          console.log(`Objeto ${clave} :  { `);
+          //Esto es una llamada recursiva a la función de sacar el objeto como parámetro para reutilizar el código que se ha hecho.
+          mostrarObjeto(valor);
+          console.log(` }`);
+        } else if (tipoDato === "function") {
+          console.log(
+            `${clave} : ${valor.name || "función anónima"}`
+          );
+        } else {
+          console.log(`${clave} : ${valor}`);
+        }
+      }
+    }
+  }
+
+
+//Función para convertir los elementos de un array a un array de objetos.
+function convertirAObjetos(array) {
+    //Hacemos un nuevo array de nombres para convertirlo a objetos, con map, añadiendole el indice como id de cada objeto.
+    let arrayNombresObjetos = [...array].map((nombre, indice) =>{
+        return {
+            id: indice,
+            nombre: nombre
+        };
+    });
+
+    console.log(`El array de objetos es el siguiente: `);
+    return mostrarObjeto(arrayNombresObjetos); //Utilizo la función de mostrar para que aparezca la información del objeto.
+}
+
+
+export {cambiarMayusculas, ordenarAlfabetoReves, convertirAObjetos};
--- practica2.04/ejercicio01/ejercicio01.js
+++ practica2.04/ejercicio01/ejercicio01.js
@@ -3,8 +3,17 @@
 //Creamos el array con los nombres propios.
 let nombresPropios = ["Felipe", "Andres", "Diana", "Alexandra", "Diego"];
 
-import { cambiarMayusculas } from "../biblioteca/biblioteca.js" ;
+//Importamos las funciones de la biblioteca.
+import { cambiarMayusculas, ordenarAlfabetoReves, convertirAObjetos } from "../biblioteca/biblioteca.js" ;
 
+//Prueba de convertir a mayúsculas.
 console.log(cambiarMayusculas(nombresPropios));
 
+//Prueba de ordenar alfabéticamente al revés.
+console.log(ordenarAlfabetoReves(nombresPropios));
 
+//Prueba de convertir los nombres a objetos.
+convertirAObjetos(nombresPropios);
+
+//Prueba de que el array inicial no ha sido modificado.
+console.log(nombresPropios);
