2fd1584669a45ad09105e212ec60f7d05fb32eb6
usuario <correo>
2024-10-01 20:53:36 +0200

    cambios en react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Contenedor from './componentes/Contenedor.jsx';
+import Interprete from './componentes/Interprete.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Contenedor>
+        <Interprete nombre='Feo' foto='https://cdn-icons-png.flaticon.com/512/742/742836.png'>
+          Esta es la biografia del feo.
+        </Interprete>
+      </Contenedor>
     </>
   )
-}
+};
 
-export default App
+export default App;
---
+++ react/src/componentes/Interprete.jsx
@@ -0,0 +1,18 @@
+import "./Interprete.css";
+
+const Interprete = (props) => {
+    //Javascript
+    const {children, nombre, foto} = props;
+    return (
+        //JSX
+        <>
+            <div className="interprete-contenedor">
+            <img className="interprete.imagen" src={foto} alt='Feo' />
+            <h1>{nombre}</h1>
+            <h3>{children}</h3>
+            </div>
+        </>
+    );
+};
+
+export default Interprete;
