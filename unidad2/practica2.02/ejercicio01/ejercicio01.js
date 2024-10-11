fe8fc5cce145f7386add05a251e41fd09b14c1e2
usuario <correo>
2024-10-11 18:23:57 +0200

    Creación de carpetas de unidad y cambio de ubicación de archivos

---
+++ unidad2/practica2.02/ejercicio01/ejercicio01.js
@@ -0,0 +1,33 @@
+"use strict"
+
+//Creamos la función sin especificar parámetros para que pueda ponerse o ninguno o muchos.
+function sumarNumeros() {
+    //usando argumentos verificamos si han pasado más de un parámetro
+    if(arguments.length < 2){
+        console.log(`Error!! Necesitas mínimo más de un número para poder realizar la suma.`);
+        return;
+    }
+
+    let suma = 0;
+
+    //Realizamos un for para leer cada parámetro introducido.
+    for(let i=0; i < arguments.length ; i++){
+
+        //verificamos si los parámetros introducidos son números o no antes de realizar la suma.
+        if(isNaN(arguments[i])){
+            console.error(`Error!! El valor ${arguments[i]} introducido no es un número.`);
+            return;
+        }
+
+        suma += arguments[i];
+    }
+
+    console.log(`La suma de los números es: ${suma}`);
+}
+
+//Hacemos una prueba con el primer error.
+sumarNumeros(1);
+//Hacemos una prueba para que salga correctamente.
+sumarNumeros(2,5,8,9);
+//hacemos una prueba metiendo un valor que no sea un número.
+sumarNumeros("Hola", 3 , "pepito");
