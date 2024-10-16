e789f8f4d497fb7a3b154afa7200b5ac2b6e903a
usuario <correo>
2024-10-16 20:43:42 +0200

    Cambios en el ejercicio 2

--- unidad3/Práctica3.02/biblioteca/biblioteca.js
+++ unidad3/Práctica3.02/biblioteca/biblioteca.js
@@ -1,9 +1,44 @@
 "use strict";
 
 function censurarPalabra(palabra){
-
-    var textoCensurado = document.body.innerHTML.replaceAll(palabra, "<span class='censura'>Contenido Bloqueado</span>");
+    //COnvertimos todo el contenido del body en string, buscamos la palabra deseada y lo cambiamos por el span.
+    var textoCensurado = document.body.innerHTML.replaceAll(palabra, "<span class='censura'>Contenido Bloqueado</span>"); 
+    //Indicamos el texto que habia anteriormente y el decimos que sea el que hemos guardado en la variable.
     document.body.innerHTML = textoCensurado;
 }
 
-export {censurarPalabra};
+function crearTabla(elemento) {
+    //Creamos la tabla y le ponemos la propiedad que deseamos.
+    const tabla = document.createElement("table");
+    tabla.setAttribute("class","tabla-propiedad");
+
+    //Creamos las filas y columnas , le damos el atributo deseado y colocamos los números dentro
+    let numero = 1;
+    for (let i = 0; i < 10; i++) {
+        const fila = document.createElement("tr");
+
+
+        for (let j = 0; j < 10; j++) {
+            const celda = document.createElement("td");
+            celda.setAttribute("class", "celda-propiedad");
+
+            celda.textContent= numero++;
+
+            fila.appendChild(celda);
+        }
+        tabla.appendChild(fila);
+    }
+
+    elemento.appendChild(tabla);
+
+}
+
+function esPrimo(numero){
+    if(numero <= 1) return false;
+    if(numero === 2) return true;
+    for (let i = 2; i < Math.sqrt(numero); i++) { //Math.sqrt devuelve la raíz cuadrada de un número.
+        if(numero % i === 0) return false; //Después de que cumple la especificación del for verifica si el resto del número es cero para descartar que es un número primo.
+    }
+    return true;//Si el resto del número no es 0 sale y devuelve este true.
+}
+export {censurarPalabra, crearTabla, esPrimo};
--- unidad3/Práctica3.02/ejercicio02/ejercicio02.js
+++ unidad3/Práctica3.02/ejercicio02/ejercicio02.js
@@ -1,1 +1,17 @@
 "use strict";
+
+import { crearTabla, esPrimo } from "../biblioteca/biblioteca.js";
+
+const tabla = document.getElementById("tabla-contenedor");
+crearTabla(tabla);
+
+const idSetTimeOut = setTimeout(()=>{
+    const celdas = document.querySelectorAll("td");
+
+    celdas.forEach((celda)=>{
+        const numero = parseInt(celda.textContent);//Obtenemos el número de la celda, ya en el html no es un número si no que es un string.
+        if(esPrimo(numero)){
+            celda.setAttribute("class", "numero-primo");
+        }
+    });
+}, 2000);
