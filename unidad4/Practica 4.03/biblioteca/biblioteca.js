f7241e91c9d8db26224168987d96492e3cba9fce
AlexaLop01 <alexandralop107@gmail.com>
2024-11-04 19:19:32 +0100

    Cambios en los ejercicios

---
+++ unidad4/Practica 4.03/biblioteca/biblioteca.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function crearTabla(contenedor, filas, columnas){
+    const tabla = document.createElement("table");
+    
+    for(let i=0; i < filas; i++){
+        for (let j = 0; j < columnas; j++) {
+            
+            
+        }
+    }
+}
---
+++ unidad4/Practica 4.03/pintarrajea.js
@@ -0,0 +1,9 @@
+"use strict";
+
+window.onload = () =>{
+    const lienzo = document.getElementById("lienzo");
+
+    lienzo.innerHTML= `<table>
+    
+    </table`;
+}
