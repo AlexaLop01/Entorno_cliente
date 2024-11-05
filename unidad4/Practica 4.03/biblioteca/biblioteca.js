5126ba2c8dc1f095fc8e728047b4736e7994e5fa
AlexaLop01 <alexandralop107@gmail.com>
2024-11-05 19:54:26 +0100

    Cambios en el ejercicio

--- unidad4/Practica 4.03/biblioteca/biblioteca.js
+++ unidad4/Practica 4.03/biblioteca/biblioteca.js
@@ -4,9 +4,14 @@ function crearTabla(contenedor, filas, columnas){
     const tabla = document.createElement("table");
     
     for(let i=0; i < filas; i++){
+        const fila = document.createElement("tr");
         for (let j = 0; j < columnas; j++) {
-            
-            
+            const celda = document.createElement("td");
+            fila.appendChild(celda);
         }
+        tabla.appendChild(fila);
     }
+    contenedor.appendChild(tabla);
 }
+
+export {crearTabla};
--- unidad4/Practica 4.03/pintarrajea.js
+++ unidad4/Practica 4.03/pintarrajea.js
@@ -1,9 +1,34 @@
 "use strict";
+import { crearTabla } from "./biblioteca/biblioteca.js";
 
 window.onload = () =>{
     const lienzo = document.getElementById("lienzo");
-
-    lienzo.innerHTML= `<table>
+    const colores = document.getElementById("colores");
+    let colorSeleccionado = null; //Esta variable es para que coja el color que selecciones, para que después pueda pintar el lienzo del mismo color.
+    let pintar = false;
     
-    </table`;
+
+    colores.addEventListener("click", (evento)=>{
+        if(evento.target.tagName === "TD"){
+            colorSeleccionado = evento.target.getAttribute("data-color-class");
+
+            const seleccionada = colores.querySelector(".seleccionada");
+            if (seleccionada){
+                seleccionada.classList.remove("seleccionada");
+            }
+
+            evento.target.classList.add("seleccionada");
+        }
+    }, false);
+
+    crearTabla(lienzo, 50, 60);
+    
+    const tabla = document.getElementsByTagName("table");
+        
+    tabla[1].addEventListener("click", (evento)=>{
+        ;
+        
+    }, false);
+    
+
 }
