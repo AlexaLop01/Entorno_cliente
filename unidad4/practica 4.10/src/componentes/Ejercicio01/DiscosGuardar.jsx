6fa03763d41a23f368b826c9eae4ddaacc370d4b
AlexaLop01 <alexandralop107@gmail.com>
2024-11-22 20:19:23 +0100

    Crear los estados y la función de actualizar

---
+++ unidad4/practica 4.10/src/componentes/Ejercicio01/DiscosGuardar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const DiscosGuardar = () => {
+    //Javascript
+    const valoresIniciales ={
+      nombreDisco: "",
+      grupoMusical: "",
+      fechPublicacion: "",
+      tipoMusica: "",
+      localizacion: "",
+      prestado: "",
+    };
+    
+  return (
+    //JSX
+    <>
+        <div>DiscosGuardar</div>
+    
+    </>
+  )
+}
+
+export default DiscosGuardar;
