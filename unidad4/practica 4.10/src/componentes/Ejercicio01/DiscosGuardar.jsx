8db4cb0dca52ba52e867c2042e6a0c28ab3911bc
AlexaLop01 <alexandralop107@gmail.com>
2024-11-22 20:20:08 +0100

    Crear los estados y la función de actualizar

--- unidad4/practica 4.10/src/componentes/Ejercicio01/DiscosGuardar.jsx
+++ unidad4/practica 4.10/src/componentes/Ejercicio01/DiscosGuardar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const DiscosGuardar = () => {
     //Javascript
+    //Creamos una objeto con los valores iniciales vacíos.
     const valoresIniciales ={
       nombreDisco: "",
       grupoMusical: "",
@@ -10,7 +11,18 @@ const DiscosGuardar = () => {
       localizacion: "",
       prestado: "",
     };
-    
+
+    //Estado de los discos
+    const [disco, setDisco] = useState(valoresIniciales);
+
+    const actualizarDatos = (evento) =>{
+      //Desestructuramos los elementos que necesitamos del input que entra por evento.
+      const {name, value} = evento;
+
+      setDisco({...discente, [name]: value});
+
+    }
+
   return (
     //JSX
     <>
