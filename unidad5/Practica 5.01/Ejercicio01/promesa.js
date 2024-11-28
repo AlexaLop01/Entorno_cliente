63d93ecfbab630fe8ee7b2b20b09a8d1551a3280
AlexaLop01 <alexandralop107@gmail.com>
2024-11-28 17:01:14 +0100

    Creamos el script de la actividad 1 de promesa resuelto

--- unidad5/Practica 5.01/Ejercicio01/promesa.js
+++ unidad5/Practica 5.01/Ejercicio01/promesa.js
@@ -1,1 +1,28 @@
 "use strict";
+
+window.onload = ()=>{
+    //Creamos la promesa, con los dos parámetros de resolver y rechazar.
+    const promesa = new Promise((resolve, reject)=>{
+        //Creamos el setTimeout donde pondremos la función que vamos a realizar de número aleatorio entre 0 y 100.
+        const idSetTimeOut = setTimeout(()=>{
+            const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+            if(numeroAleatorio % 2 === 0){
+                //Colocamos que devuelve el caso de que resuelva.
+                resolve(numeroAleatorio);
+            }else{
+                //Colocamos el error que queremos que salga cuando sea rechazado.
+                reject(new Error(`El valor del número aleatorio es impar: ${numeroAleatorio}`));
+            }
+        }, 1000);
+    })
+
+    //En esta parte usamos el then y el catch para que salgan las respuestas adquiridas en la función de la promesa creada anteriormente.
+    promesa.then((datos)=>{
+        //Si la respuesta es resuelta aparece este log.
+        console.log(`El valor del número aleatorio es par: ${datos}`);
+    })
+    .catch((error) =>{
+        //Si la respuesta se trata de un error aparece este log.
+        console.log(error.message);
+    })
+}
