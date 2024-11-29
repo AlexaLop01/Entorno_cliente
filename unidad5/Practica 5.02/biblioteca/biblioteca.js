f9cc3702815a727a68c51276818c6c31ff94f559
AlexaLop01 <alexandralop107@gmail.com>
2024-11-29 20:30:09 +0100

    Creamos la función de mostrar la pelicula y la selección del contenedor de las películasç

--- unidad5/Practica 5.02/biblioteca/biblioteca.js
+++ unidad5/Practica 5.02/biblioteca/biblioteca.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//Creamos una función para mostrar el listado de las películas.
+const mostrarListadoPeliculas = (datos, contenedor)=>{
+    //Creamos el elemento ul.
+    const lista = document.createElement("ul");
+
+    datos.map((pelicula)=> {
+        lista.innerHTML += `<li>Id: ${pelicula.episode_id} 
+        titulo: ${pelicula.title} </li>`;
+    })
+
+    contenedor.appendChild(lista)
+}
+
+//Crear una función para mostrar la información de la película seleccionada.
+
+
+export {mostrarListadoPeliculas};
--- unidad5/Practica 5.02/enciclopedia.js
+++ unidad5/Practica 5.02/enciclopedia.js
@@ -1,1 +1,8 @@
 "use strict";
+
+window.onload = ()=>{
+    //Seleccionamos el contenedor donde se guardará la lista de películas.
+    const contendorPeliculas = document.getElementById("contenedor-peliculas");
+
+    //Creamos la inserción de la lista de peliculas.
+}
