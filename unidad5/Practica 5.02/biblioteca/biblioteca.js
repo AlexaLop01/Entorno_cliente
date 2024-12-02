4e437a7db432b341e88e6f0c64fe8d54892f9af6
AlexaLop01 <alexandralop107@gmail.com>
2024-12-02 18:59:11 +0100

    Creación de las funciones que muestran las películas y la implementación en el javascript del ejercicio

--- unidad5/Practica 5.02/biblioteca/biblioteca.js
+++ unidad5/Practica 5.02/biblioteca/biblioteca.js
@@ -5,15 +5,37 @@ const mostrarListadoPeliculas = (datos, contenedor)=>{
     //Creamos el elemento ul.
     const lista = document.createElement("ul");
 
-    datos.map((pelicula)=> {
-        lista.innerHTML += `<li> 
-        ${pelicula.episode_id} ${pelicula.title} </li>`;
+    datos.map((peliculas)=> {
+        lista.innerHTML += `<li id="${peliculas.episode_id}"> 
+        ${peliculas.episode_id} : ${peliculas.title} </li>`;
     })
 
     contenedor.appendChild(lista);
-}
+};
 
 //Crear una función para mostrar la información de la película seleccionada.
+//Haremos una función para formatear la fecha que nos pasa la api.
+const formatearFechaEuropeo = (fecha)=>{
+    const [year, month, day] = fecha.split("-");
+    return `${day}/${month}/${year}`;
+}
+//Primero creamos una función que haga un map de una pelicula en concreto que usaremos después en la función que la filtra.
+const mapearInformacionPelicula = (peliculaFiltrada, contenedor)=>{
+    peliculaFiltrada.map((pelicula)=>{
+        contenedor.innerHTML = `<h3>${pelicula.title}</h3>
+        <p><strong>Director:</strong> ${pelicula.director}</p>
+        <p><strong>Productor:</strong> ${pelicula.producer}</p>
+        <p><strong>Fecha de lanzamiento:</strong> ${formatearFechaEuropeo(pelicula.release_date)}</p>
+        <p><strong>Sinopsis:</strong>  ${pelicula.opening_crawl}</p>`;
 
+    })
+}
+//Segundo creamos una función que te permita filtrar los datos y los pinte por pantalla.
+const mostrarInformacionPelicula = (datos,identificador, contenedor) =>{
+    const peliculaFiltrada = datos.filter((pelicula)=>{
+        return pelicula.episode_id === parseInt(identificador);
+    })
+    mapearInformacionPelicula(peliculaFiltrada, contenedor);
+};
 
-export {mostrarListadoPeliculas};
+export {mostrarListadoPeliculas, mostrarInformacionPelicula};
--- unidad5/Practica 5.02/enciclopedia.js
+++ unidad5/Practica 5.02/enciclopedia.js
@@ -1,13 +1,13 @@
 "use strict";
 
-import { mostrarListadoPeliculas } from "./biblioteca/biblioteca.js";
+import { mostrarInformacionPelicula, mostrarListadoPeliculas } from "./biblioteca/biblioteca.js";
 
 window.onload = ()=>{
     //Seleccionamos el contenedor donde se guardará la lista de películas.
     //Guardamos la url de la api.
-    const urlFilms = "https://swapi.dev/api/films";
+    const urlFilms = "https://swapi.dev/api/films/";
     
-    //Creamos la inserción de la lista de peliculas.
+    //Creamos la inserción de la lista de películas.
     fetch(urlFilms)
     .then((respuesta)=>{
         //Convierte en un json lo que le pasamos por fetch.
@@ -17,10 +17,20 @@ window.onload = ()=>{
         //Coge lo que respuesta le pasa y hacemos un mapeado para poder sacarlo por pantalla.
         const contendorPeliculas = document.getElementById("contenedor-peliculas");
         mostrarListadoPeliculas(datos.results, contendorPeliculas);
+
+        //Seleccionamos el contenedor donde se mostrará la información de la película.
+        const contenedorInformacion = document.getElementById("contenedor-información");
+
+        contendorPeliculas.addEventListener("click", (evento)=>{
+            if(evento.target.tagName === "LI"){
+                //Seleccionamos la película en concreto y la mostramos.
+                mostrarInformacionPelicula(datos.results, evento.target.id, contenedorInformacion);
+            }
+        }, false);
     
     })
     .catch((error)=>{
         //Hacemos el control de errores. 
-        console.error(error);;
+        console.log(`Ha ocurrido un error: ${error.message}`);
     })
 }
