1beee432efe9f6585830904d2dcc13ee785c1e66
AlexaLop01 <alexandralop107@gmail.com>
2024-12-07 15:10:16 +0100

    Cambios en el estilo de los componentes

--- unidad5/practica 5.03/src/App.jsx
+++ unidad5/practica 5.03/src/App.jsx
@@ -1,10 +1,12 @@
+
 import './App.css';
 import ListadoPeliculas from './componentes/ListadoPeliculas.jsx';
 
 function App() {
-
+  //Javascript
 
   return (
+    //JSX
     <>
       <ListadoPeliculas/>
     </>
--- unidad5/practica 5.03/src/componentes/mostrarListado/Pelicula.jsx
+++ unidad5/practica 5.03/src/componentes/mostrarListado/Pelicula.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import "./Pelicula.css";
 
 const Pelicula = ({peliculaAMostrar}) => {
   //Javascript
   return (
     //JSX
     <>
-      <h3 id={peliculaAMostrar.episode_id}>
-        {peliculaAMostrar.episode_id} : {peliculaAMostrar.title}
-      </h3>
+      <div className='contendor-pelicula-lista'>
+        <h3 id={peliculaAMostrar.episode_id}>
+          {peliculaAMostrar.episode_id} : {peliculaAMostrar.title}
+        </h3>
+      </div>
     </>
   )
 }
