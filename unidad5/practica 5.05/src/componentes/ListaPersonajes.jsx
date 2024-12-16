2b886ee3b602a8f196a20bc185e1afc8c1e6ab36
AlexaLop01 <alexandralop107@gmail.com>
2024-12-16 14:04:25 +0100

    Creación de un nuevo ejercicio

---
+++ unidad5/practica 5.05/src/componentes/ListaPersonajes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ListaPersonajes = () => {
+    const url = "https://rickandmortyapi.com/api/";
+    /**
+     Todos los personajes: https://rickandmortyapi.com/api/character/
+    Episodios: https://rickandmortyapi.com/api/episode/
+    Localizaciones: https://rickandmortyapi.com/api/location/
+     */
+  return (
+    <div>ListaPersonajes</div>
+  )
+}
+
+export default ListaPersonajes;
